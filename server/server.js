165afc0d61aa9385c0fe7665850da73686ab6ec0
Taras Bukachevskyy <Taras@levy-ve505-0321.apn.wlan.upenn.edu>
2018-04-16 10:45:37 -0400

    commit

--- server/server.js
+++ server/server.js
@@ -7,15 +7,10 @@ const Event = require('/models/events');
 
 //const config = require('./config');
 
-// TODO: connect to our database via mongoose.connect. Grab the url from config.database
-// STUB
 mongoose.connect(process.env.MONGODB_URI || config.database);
-// ENDSTUB
 
-// load promisified functions into mongoose. Refer to
-// http://erikaybar.name/using-es6-promises-with-mongoosejs-queries/ for more info
-// TODO: Read that article. Our tests assume you're returning promises in your database
-// queries!
+
+
 mongoose.Promise = global.Promise;
 
 // instantiate express app
@@ -24,9 +19,7 @@ const app = express();
 // set the superSecret key in our app which we will use  to sign our jwts
 //app.set('superSecret', config.secret);
 
-// instantiate bodyParser middleware so we can get fields from post requests via req.body.fieldName
 app.use(bodyParser.urlencoded({ extended: true }));
-// instatiate bodyParser to also handle requests with JSON and not just form data
 app.use(bodyParser.json());
 
 // static directory setup
